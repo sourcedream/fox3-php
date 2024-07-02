1414631d6145fffaef0976bd2b1f0175542ea8c5
sipmann <sipmann@gmail.com>
2024-07-02 11:43:59 -0400

    MDFE funcoes

--- Controllers/MDFEController.php
+++ Controllers/MDFEController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Fox3\Controller;
+use App\Fox3\Helpers\Mysql;
 
 class MDFEController extends Controller {
 
@@ -12,5 +13,32 @@ class MDFEController extends Controller {
 
     public function closeMDFE() {
         $barcode = $_REQUEST['barcode'];
+
+        $mdfe = $this->findMdfe($barcode);
+
+        if (is_null($mdfe) || !$mdfe) {
+            return $this->view('mdfe/close.php', ['aviso' => 'MDF-e não localizado na base de dados']);
+        }        
+
+        return $this->redirect('/mdfe-encerrado');
+    }
+
+    public function mdfeClosed() {
+        return $this->view('mdfe/closed.php');
+    }
+
+    private function getMFDEs() {
+        $con = Mysql::conn();
+        $stm = $con->prepare('SELECT * FROM MDFE');
+        $stm->execute();
+        return $stm->get_result()->fetch_all(MYSQLI_ASSOC);
+    }
+
+    private function findMdfe(string $barcode) {
+        $con = Mysql::conn();
+        $stm = $con->prepare('SELECT * FROM MDFE WHERE chave = ?');
+        $stm->bind_param('s', $barcode);
+        $stm->execute();
+        return $stm->get_result()->fetch_all(MYSQLI_ASSOC);
     }
 }
